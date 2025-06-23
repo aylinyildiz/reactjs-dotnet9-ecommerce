bc6f4cd0e1ec56a518e0bb2ea28814000195ba47
Aylin Yıldız <aylin.yildiz@blfoptik.com>
2025-06-23 14:26:44 +0300

    first commit

---
+++ Client/src/pages/catalog/Product.tsx
@@ -0,0 +1,30 @@
+import { AddShoppingCart } from "@mui/icons-material";
+import { Button, Card, CardActions, CardContent, CardMedia, Typography } from "@mui/material";
+import { IProduct } from "../../model/IProduct";
+import SearchIcon from '@mui/icons-material/Search';
+import { Link } from "react-router";
+
+interface Props {
+  product: IProduct;
+}
+
+export default function Product({ product }: Props) {
+  return (
+    <Card>
+        <CardMedia sx={{ height: 160, backgroundSize:"contain" }} image={`http://localhost:5159/images/${product.imageUrl}`}/>
+        <CardContent>
+            <Typography gutterBottom variant="h6" component="h2" color="text-secondary">
+                {product.name}
+            </Typography>
+            <Typography variant="body2" color="secondary">
+                {(product.price /100).toFixed(2)} ₺
+            </Typography>
+        </CardContent>
+        <CardActions>
+            <Button variant="outlined" size="small" startIcon={<AddShoppingCart />} color="success">Add to Cart</Button>
+            <Button component={Link} to={`/catalog/${product.id}`} variant="outlined" size="small" startIcon={<SearchIcon />} color="primary">View</Button>
+        </CardActions>
+    </Card>
+ 
+  );
+}
---
+++ Client/src/router/Routes.tsx
@@ -0,0 +1,29 @@
+import { createBrowserRouter } from "react-router";
+import App from "../components/App";
+import HomePage from "../pages/HomePage";
+import AboutPage from "../pages/AboutPage";
+import ContactPage from "../pages/ContactPage";
+import CatalogPage from "../pages/catalog/CatalogPage";
+import ProductDetailsPage from "../pages/catalog/ProductDetails";
+import ErrorPage from "../pages/ErrorPage";
+import ServerError from "../errors/ServerError";
+import NotFound from "../errors/NotFound";
+import { Navigate } from "react-router";
+
+export const router = createBrowserRouter([
+    { 
+        path: "/",
+        element: <App/>,
+        children: [
+            {path:"", element: <HomePage/>},
+            {path:"about", element: <AboutPage/>},
+            {path:"contact", element: <ContactPage/>},
+            {path:"catalog", element: <CatalogPage/>},
+            {path:"catalog/:id", element: <ProductDetailsPage/>}, 
+            {path:"error", element: <ErrorPage/>},
+            {path:"server-error", element: <ServerError/>},
+            {path:"not-found", element: <NotFound />},
+            {path: "*", element: <Navigate to="/not-found" />}
+        ]
+    }
+])
